4afb8edfe82187c86bc134e602d9bae3bd57a4b0
Justin Thomas <justin@justinthomas.name>
2012-07-22 15:07:59 -0700

    updated to 3.4.1

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.4';
+$wp_version = '3.4.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 20596;
+$wp_db_version = 21115;
 
 /**
  * Holds the TinyMCE version
